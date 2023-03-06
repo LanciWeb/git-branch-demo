5f474f98d82e172d735b80430c21d77973946163
DarioGiusino <dario.giusino@gmail.com>
2023-03-06 11:34:19 +0100

    added h1

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -1,15 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
+@extends('layouts.main')
 
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
-</head>
-
-<body>
-    <h1>Benvenuti</h1>
-</body>
-
-</html>
+@section('content')
+  <h1>ciaone</h1>
+@endsection
